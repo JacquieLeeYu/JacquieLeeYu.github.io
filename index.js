86194a5f4c3667b7d595b19cabf4188e755763b7
JacquieLeeYu <yu790@purdue.edu>
2022-09-12 12:56:28 -0700

    Shortened functions in index.js
    I used a lot of repetitive functions previously, but now it's all cleaned up to one function (though it's still kind of long).

--- index.js
+++ index.js
@@ -1,86 +1,113 @@
 function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    const dateString = new Date().toLocaleTimeString();
-    timeDisplay.textContent = dateString;
-  }
-
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleTimeString();
+  timeDisplay.textContent = dateString;
+}
 const slideshowImagesNum = 21;
-var currentSlideshowIndex = getRndInteger(0, slideshowImagesNum - 1);
-
 const slideshowElement = document.getElementById('slideshow');
 
-function renderSlideshowImage() {
-    let currentImagePath = "./pictures/slideshow/" + currentSlideshowIndex + ".jpg";
-    slideshowElement.style.backgroundImage = "url('" + currentImagePath +"')";
-    const isLastPhoto = currentSlideshowIndex === slideshowImagesNum - 1;
-    currentSlideshowIndex = isLastPhoto ? 0 : currentSlideshowIndex + 1;
-}
+const zooImagesNum = 14;
+const zooctx = document.getElementById('smallSlideshow');
+
+const japanImagesNum = 16;
+const japanctx = document.getElementById('smallSlideshowJapan');
+
+const purdueImagesNum = 16;
+const purduectx = document.getElementById('smallSlideshowPurdue');
+
+const chicagoImagesNum = 11;
+const chicagoctx = document.getElementById('smallSlideshowChicago');
+
+var slideshowShowing;
+let zooShowing;
+let japanShowing;
+let purdueShowing;
+let chicagoShowing;
+
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-const zooImagesNum = 13;
-var currentZooIndex = getRndInteger(0, zooImagesNum - 1);
-const zooctx = document.getElementById('smallSlideshow');
-function renderZooImage(place) {
-  let currentZooPath = "./pictures/zoo/" + currentZooIndex + ".jpg";
-  zooctx.style.backgroundImage = "url('" + currentZooPath +"')";
-  const isLastZooPhoto = currentZooIndex === zooImagesNum - 1;
-  currentZooIndex = isLastZooPhoto ? 0 : currentZooIndex + 1;
+function renderImage(name) {
+  let infoArray = getEssentialInfo(name);
+  let elem = infoArray[0],
+      imgPath = infoArray[1],
+      imgNum = infoArray[2];
+  if (name == "slideshow") {
+    let currentPath = imgPath + slideshowIndex + ".jpg";
+    elem.style.backgroundImage = "url('" + currentPath +"')";
+    let isLastPhoto = slideshowIndex == imgNum - 1;
+    slideshowIndex = isLastPhoto ? 0 : slideshowIndex + 1;
+  } else if (name == "zoo") {
+    let currentPath = imgPath + zooIndex + ".jpg";
+    elem.style.backgroundImage = "url('" + currentPath +"')";
+    let isLastPhoto = zooIndex == imgNum - 1;
+    zooIndex = isLastPhoto ? 0 : zooIndex + 1;
+  } else if (name == "japan") {
+    let currentPath = imgPath + japanIndex + ".jpg";
+    elem.style.backgroundImage = "url('" + currentPath +"')";
+    let isLastPhoto = japanIndex == imgNum - 1;
+    japanIndex = isLastPhoto ? 0 : japanIndex + 1;
+  } else if (name == "purdue") {
+    let currentPath = imgPath + purdueIndex + ".jpg";
+    elem.style.backgroundImage = "url('" + currentPath +"')";
+    let isLastPhoto = purdueIndex == imgNum - 1;
+    purdueIndex = isLastPhoto ? 0 : purdueIndex + 1;
+  } else if (name == "chicago") {
+    let currentPath = imgPath + chicagoIndex + ".jpg";
+    elem.style.backgroundImage = "url('" + currentPath +"')";
+    let isLastPhoto = chicagoIndex == imgNum - 1;
+    chicagoIndex = isLastPhoto ? 0 : chicagoIndex + 1;
+  }
 }
 
-const japanImagesNum = 15;
-var currentJapanIndex = getRndInteger(0, japanImagesNum - 1);
-const japanctx = document.getElementById('smallSlideshowJapan');
-function renderJapanImage() {
-  let currentJapanPath = "./pictures/japan/" + currentJapanIndex + ".jpg";
-  japanctx.style.backgroundImage = "url('" + currentJapanPath +"')";
-  const isLastJapanPhoto = currentJapanIndex === japanImagesNum - 1;
-  currentJapanIndex = isLastJapanPhoto ? 0 : currentJapanIndex + 1;
-}
-
-const purdueImagesNum = 15;
-var currentPurdueIndex = getRndInteger(0, purdueImagesNum - 1);
-const purduectx = document.getElementById('smallSlideshowPurdue');
-function renderPurdueImage() {
-  let currentPurduePath = "./pictures/purdue/" + currentPurdueIndex + ".jpg";
-  purduectx.style.backgroundImage = "url('" + currentPurduePath +"')";
-  const isLastPurduePhoto = currentPurdueIndex === purdueImagesNum - 1;
-  currentPurdueIndex = isLastPurduePhoto ? 0 : currentPurdueIndex + 1;
-}
-
-const chicagoImagesNum = 10;
-var currentChicagoIndex = getRndInteger(0, chicagoImagesNum - 1);
-const chicagoctx = document.getElementById('smallSlideshowChicago');
-function renderChicagoImage() {
-  let currentChicagoPath = "./pictures/chicago/" + currentChicagoIndex + ".jpg";
-  chicagoctx.style.backgroundImage = "url('" + currentChicagoPath +"')";
-  const isLastChicagoPhoto = currentChicagoIndex === chicagoImagesNum - 1;
-  currentChicagoIndex = isLastChicagoPhoto ? 0 : currentChicagoIndex + 1;
+// Returns element, beginning images' path, and # of images
+function getEssentialInfo (name) {
+  if (name == "slideshow") {
+    let returnArray = [slideshowElement, "./pictures/slideshow/", slideshowImagesNum];
+    return returnArray;
+  } else if (name == "zoo") {
+    let returnArray = [zooctx, "./pictures/zoo/", zooImagesNum];
+    return returnArray;
+  } else if (name == "japan") {
+    let returnArray = [japanctx, "./pictures/japan/", japanImagesNum];
+    return returnArray;
+  } else if (name == "purdue") {
+    let returnArray = [purduectx, "./pictures/purdue/", purdueImagesNum];
+    return returnArray;
+  } else if (name == "chicago") {
+    let returnArray = [chicagoctx, "./pictures/chicago/", chicagoImagesNum];
+    return returnArray;
+  } else {
+    return "It dun work son"
+  }
 }
 
 
+var slideshowIndex = getRndInteger(0, slideshowImagesNum - 1);
+var zooIndex = getRndInteger(0, zooImagesNum - 1);
+var japanIndex = getRndInteger(0, japanImagesNum - 1);
+var purdueIndex = getRndInteger(0, purdueImagesNum - 1);
+var chicagoIndex = getRndInteger(0, chicagoImagesNum - 1);
 
-let slideshowShowing;
-let zooShowing;
-let japanShowing;
-let purdueShowing;
-let chicagoShowing;
 if ( document.URL.includes("photography") ) {
-  renderSlideshowImage();
-  slideshowShowing = setInterval(renderSlideshowImage, 8000);
-  renderZooImage();
-  zooShowing = setInterval(renderZooImage, 8000);
-  renderJapanImage();
-  japanShowing = setInterval(renderJapanImage, 8000);
-  renderPurdueImage();
-  purdueShowing = setInterval(renderPurdueImage, 8000);
-  renderChicagoImage();
-  chicagoShowing = setInterval(renderChicagoImage, 8000);
+  renderImage("slideshow");
+  renderImage("zoo");
+  renderImage("japan");
+  renderImage("purdue");
+  renderImage("chicago");
+  slideshowShowing = setInterval(renderImage, 6000, "slideshow");
+  zooShowing = setInterval(renderImage, 6000, "zoo");
+  japanShowing = setInterval(renderImage, 6000, "japan");
+  purdueShowing = setInterval(renderImage, 6000, "purdue");
+  chicagoShowing = setInterval(renderImage, 6000, "chicago");
 } else if (slideshowShowing) {
   clearInterval(slideshowShowing);
   clearInterval(zooShowing);
+  clearInterval(japanShowing);
+  clearInterval(purdueShowing);
+  clearInterval(chicagoShowing);
 }
 
 let timeShowing;
